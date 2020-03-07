f5e4bfca938720652cd3379412a49cc635ea2844
Dev-kris <corpkris@gmail.com>
2020-03-07 15:16:51 +0100

    updated version with new countries and added company names

--- web_scrape.py
+++ web_scrape.py
@@ -1,7 +1,28 @@
 from urllib.request import urlopen as uReq
 from bs4 import  BeautifulSoup as soup
 
-my_url = "https://www.indeed.fr/jobs?q=english&l=Paris+%2875%29&sort=date"
+terms_input = input("Enter Search Terms ").lower()
+country_input = input ("Enter Country ").lower()
+location_input = input("Enter Location ").lower()
+
+#append correct country code to url
+for country_code in country_input:
+    country_code = []
+    if country_input == "france":
+            country_code = ".fr/jobs?q="
+    elif country_input == "portugal":
+        country_code = ".pt/ofertas?q="
+    elif country_input == "spain":
+        country_code = ".es/ofertas?q="
+    elif country_input == "germany":
+        country_code = ".pt/ofertas?q="
+    elif country_input == "italy":
+        country_code = ".com/offerte-lavoro?q="
+    else:
+        print ("Sorry " +country_input + " is not supported yet")
+        break
+
+my_url = "https://www.indeed" + country_code + terms_input + "&l=" + location_input + "&sort=date"
 
 #open and download the page
 uClient = uReq(my_url)
@@ -16,8 +37,17 @@ containers = page_soup.findAll("div",{"class":"jobsearch-SerpJobCard"})
 
 
 
-# print to test containers is scraping correctly
+# print each job title and company name
 for container in range(len(containers)):
     title_container = containers[container].find("div",{"class": "title"})
+    company_container = containers[container].find("span",{"class": "company"})
+    #location_container = containers[container].find("div",{"class": "location"})
     print("Job Title: " + title_container.text.strip())
+    print("Company:   " + company_container.text.strip())
+    #print("Location:  " + location_container.text.strip())
+    print(" ")
+
     
+#for container in range(len(containers)):
+    #company_container = containers[container].find("div",{"class": "sjcl"})
+    #print("Company: " + company_container.text.strip())
